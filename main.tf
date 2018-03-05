18a607775144df172d964f21b19fcd34fb8c5ad4
Nic Jackson <jackson.nic@gmail.com>
2018-03-05 22:03:26 +0000

    Added workers and join cluster

--- main.tf
+++ main.tf
@@ -27,11 +27,50 @@ resource "digitalocean_droplet" "k8s_master" {
 
     inline = [
       # init k8s and bind to private ip address
-      "sudo kubeadm init --apiserver-advertise-address=${digitalocean_droplet.k8s_master.ipv4_address_private}",
+      "sudo kubeadm init --token=${var.k8s_token} --apiserver-advertise-address=${digitalocean_droplet.k8s_master.ipv4_address_private}",
+
+      "kubectl get pods --all-namespaces",
     ]
   }
 }
 
-output "web_ip" {
+resource "digitalocean_droplet" "k8s_worker" {
+  count = 2
+
+  image              = "${var.image_id}"
+  name               = "k8s-worker-${count.index}"
+  region             = "lon1"
+  size               = "4gb"
+  private_networking = true
+
+  ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
+
+  # bring up the kube master on the master node
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = "root"
+    }
+
+    inline = [
+      # init k8s and bind to private ip address
+      "sudo kubeadm join --token=${var.k8s_token} --discovery-token-unsafe-skip-ca-verification ${digitalocean_droplet.k8s_master.ipv4_address_private}:6443",
+    ]
+  }
+}
+
+output "web_ip_k8s_master" {
   value = "${digitalocean_droplet.k8s_master.ipv4_address}"
+}
+
+output "web_ip_k8s_master_privage" {
+  value = "${digitalocean_droplet.k8s_master.ipv4_address_private}"
+}
+
+output "web_ip_k8s_workers" {
+  value = "${digitalocean_droplet.k8s_worker.*.ipv4_address}"
+}
+
+output "web_ip_k8s_workers_private" {
+  value = "${digitalocean_droplet.k8s_worker.*.ipv4_address_private}"
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "image_id" {
   description = "Digital Ocean Image ID"
   default     = "32247739"
 }
+
+variable "k8s_token" {
+  description = "K8s token to use for joining clusters"
+  default     = "b5aa6e.c74e8c9996726092"
+}
