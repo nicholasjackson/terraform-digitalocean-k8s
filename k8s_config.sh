fb0c6d6b62462963125dc4c48223b125ec118b23
Nic Jackson <jackson.nic@gmail.com>
2018-03-16 08:29:57 +0000

    Updated helper script

--- k8s_config.sh
+++ k8s_config.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 function tunnel_k8s_api {
-  ssh -i $1 -f root@$(terraform output k8s_master_public_ip) -L 6443:$(terraform output web_ip):6443 -N
+  ssh -i $(terraform output k8s_private_key) -f root@$(terraform output k8s_master_public_ip) -L 6443:$(terraform output k8s_master_public_ip):6443 -N
+
+  if [ $? != 0 ]; then
+    exit $?
+  fi
 
   echo "Started SSH tunnel to K8s master, the API is now available at https://kubernetes:6443"
 }
 
 function get_k8s_config {
   mkdir -p $HOME/.kube 
-  scp -i $1 root@$(terraform output k8s_master_public_ip):/etc/kubernetes/admin.conf $HOME/.kube/do-k8s
+  scp -i $(terraform output k8s_private_key) root@$(terraform output k8s_master_public_ip):/etc/kubernetes/admin.conf $HOME/.kube/do-k8s
   KUBECONFIG=$HOME/.kube/do-k8s kubectl config set-cluster kubernetes --server=https://kubernetes:6443
 
   echo "Kubernetes config downloaded and stored at $HOME/.kube/do-k8s"
@@ -15,10 +19,12 @@ function get_k8s_config {
 
 function print_usage {
   echo "Useage instructions:"
-  echo "  k8s_config.sh [command] -i [path to ssh private key]"
+  echo "  k8s_config.sh [command]"
   echo "    commands:"
   echo "      tunnel    : create a ssh tunnel to the k8s api"
   echo "      get-config: fetch the kubernetes config"
+  echo ""
+  echo "NOTE: IP addresses and the path to the private key will be picked up from the terraform output"
   echo ""
   exit 2
 }
@@ -28,32 +34,13 @@ if [ $# -eq 0 ]; then
 fi
 
 command=$1
-shift
-
-ssh_key=""
-while getopts "i:" OPTION
-do
-	case $OPTION in
-    i)
-      ssh_key=$OPTARG
-      ;;
-    \?)
-      print_usage
-      ;;
-  esac
-  shift
-done
-
-if [[ $ssh_key == "" ]]; then
-  print_usage
-fi
 
 case $command in
   tunnel)
-    tunnel_k8s_api $ssh_key
+    tunnel_k8s_api
     ;;
   get-config)
-    get_k8s_config $ssh_key
+    get_k8s_config
     ;;
 esac
 
--- output.tf
+++ output.tf
@@ -13,3 +13,11 @@ output "k8s_workers_public_ip" {
 output "k8s_workers_private_ip" {
   value = "${digitalocean_droplet.k8s_worker.*.ipv4_address_private}"
 }
+
+output "k8s_private_key" {
+  value = "${var.ssh_private_key}"
+}
+
+output "k8s_public_key" {
+  value = "${var.ssh_public_key}"
+}
